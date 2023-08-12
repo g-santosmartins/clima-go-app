b74a2c61f9ceb2a6885098cc6ce7d65b9f433263
Guilherme Martins <g.santosmartins@outlook.com>
2023-08-12 13:41:01 -0300

    refactor: :recycle: remove from home weather api call responsability

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,29 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { apiGet } from "../../controllers/apihandler"
+
 import { HomeContainer } from "./styles"
+
 import { Header } from "@components/Header"
 import { WeatherContainer } from "@components/WeatherContainer"
 import { NotificationButton } from "@components/NotificationButton"
 import { WeatherStatusIcon } from "@components/WheatherStatusIcon"
 import { WeatherForecastChart } from "@components/WeatherForecastChart"
 import { RootContainer } from "@components/common/Container/styles"
+import handleGetEnvironmentVariables from "../../../env"
+import { handleGetWeatherData } from "@utils/ApiHandlerLimiter"
 
 export function Home() {
+  useEffect(() => {
+    // handleGetWeatherData("Marília", "metric").then((response) => {
+    //   console.log("RETURN", response)
+    // })
+
+  });
   return (
     <RootContainer>
       <Header />
       <HomeContainer>
-        <WeatherStatusIcon />
+        <WeatherStatusIcon imageAlias="rainy"/>
         <WeatherContainer />
         <NotificationButton />
         <WeatherForecastChart />
