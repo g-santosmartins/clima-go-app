c1249d17c83741e5a1d099059323402c04af70ef
Guilherme Martins <g.santosmartins@outlook.com>
2023-08-12 12:11:28 -0300

    feat: :sparkles: add weather status icon integration

--- src/components/WheatherStatusIcon/index.tsx
+++ src/components/WheatherStatusIcon/index.tsx
@@ -1,9 +1,32 @@
-import { Image } from "react-native"
+import { Image, ImageSourcePropType } from "react-native"
+import { WeatherStatusIconContainer } from "./styles"
 
+interface WeatherStatusIconProps {
+  imageUrl: keyof IconButtonProps
+}
 
-export const WeatherStatusIcon = () => {
+interface IconButtonProps {
+  sunny: ImageSourcePropType
+  rainy: ImageSourcePropType
+  stormy: ImageSourcePropType
+  cloudy: ImageSourcePropType
+  foggy: ImageSourcePropType
+  snowy: ImageSourcePropType
+}
+
+export const WeatherStatusIcon = ({imageUrl} : WeatherStatusIconProps, ...rest : any[]) => {
+  const iconsImports : IconButtonProps = {
+    sunny: require("../../assets/Main/sunny.png"),
+    rainy: require("../../assets/Main/rainy.png"),
+    stormy: require("../../assets/Main/stormy.png"),
+    cloudy: require("../../assets/Main/cloudy.png"),
+    foggy: require("../../assets/Main/foggy.png"),
+    snowy: require("../../assets/Main/snowy.png"),
+  }
   return <>
-    <Image source={require("../../assets/Main/sunny.png")}/>
+    <WeatherStatusIconContainer>
+      <Image source={iconsImports[imageUrl]}/>
+    </WeatherStatusIconContainer>
   </>
 }
 
