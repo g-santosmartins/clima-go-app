78d4c453eba17cb2da2ffc3505ae79f8d02ba47a
Guilherme Martins <g.santosmartins@outlook.com>
2023-08-27 09:17:37 -0300

    refactor: :recycle: change gradient background colors

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,9 +2,9 @@ export function getPeriodOfDayColorGradient() {
   const currentHour = new Date().getHours();
 
   if (currentHour >= 5 && currentHour < 12) {
-    return  ["#2F5F99", '#FEB11B'];
-  } else if (currentHour >= 12 && currentHour < 18) {
     return ["#2F5F99", '#FEB07B'];
+  } else if (currentHour >= 12 && currentHour < 18) {
+    return  ["#2F5F99", '#FEB11B'];
   } else {
     return ["#000010", '#1374A7'];
   }
